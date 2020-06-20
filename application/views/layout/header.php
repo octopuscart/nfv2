6473d2a91ccc1505360f9c2bfb98fffa0fbb1f90
dev <dev@develop.com>
2020-06-20 10:22:47 +0530

    cahgnes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -246,13 +246,17 @@ if ($session_data) {
                     <div class="container-fluid">
                         <!-- Brand and toggle get grouped for better mobile display -->
                         <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" aria-label="mobilemenu" id="open_side_menu3" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <button type="button" class="navbar-toggle collapsed" aria-label="mobilemenu" id="open_side_menu3" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" style="position: absolute;
+    float: right;
+    right: 0px;
+    z-index: 2000;
+    opacity: 1;">
                                 <span class="sr-only">Toggle navigation</span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="index.php" style="color:white;    color: white;
+                            <a class="navbar-brand"  style="color:white;    color: white;
     text-align: center;
     width: 100%;
     float: left;

4466967a3102e8a25ca1f0598a54966c389efeba
dev <dev@develop.com>
2020-08-28 14:47:47 +0530

    changes

--- application/views/home.php
+++ application/views/home.php
@@ -97,6 +97,54 @@ $this->load->view('layout/header');
         </ul>
     </div>
 </section>
+
+
+<!--benifits-->
+<section class="section_offset" style='padding:0px;margin-top:10px;'>
+    <div class="container t_align_c">
+        <h3 class="color_dark fw_light m_bottom_35 heading_1" data-appear-animation="bounceInLeft" style='font-weight: 500;'>About Nita Fashions</h3>
+
+
+        <article id="tab-1" data-appear-animation="fadeInUp" data-appear-animation-delay="450">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 m_xs_bottom_20">
+
+                    <img src="<?php echo base_url(); ?>assets/images/nitaabout.jpg" alt="Nita Fashions" style='border-radius: 10px;'>
+
+
+
+                </div>
+                <div class="col-lg-6 col-md-6 t_align_l aboutsetion" style='    font-size: 18px;'>
+
+                    <p class="m_bottom_5"  >
+                        Nita Fashions has been well recognized for its superior fabrics, meticulous hand tailoring, outstanding quality and customer service since 1953.
+                    </p>
+
+                    <p class="m_bottom_5" >
+                        Its owner and Chief Tailor, Mr. Peter Daswani, recently partnered with his son Anil Daswani giving the business the perfect balance of classic and modern, taking it to its peak. 
+                    </p>
+
+                    <p class="m_bottom_5">
+                        Nita Fashions’ mission is to equip customers with the knowledge and bespoke wardrobe to attend every event with confidence. They are able to achieve this by working closely with clients and understanding their style and knowing what will flatter their form. 
+                    </p>
+
+                    <p class="m_bottom_5" >
+                        The roots of Mr. Peter Daswani’s craftsmanship are derived from the pioneer, his father - who began as an apprentice on Saville Row; as a result, his cutting style is both traditional and very British. To contrast, Anil has a very modern Italian style and technique that resembles the current running trends. 
+                    </p>
+
+                    <p class="m_bottom_5">
+                        His knowledge and understanding stems from being under the guidance of his Father, Mr. Peter Daswani, whilst over the years adding his own twist.   
+                    </p>
+
+                </div>
+        </article>
+
+
+    </div>
+</section>
+<!--end of benifits-->
+<hr/>
+
 <!--content-->
 <section class="section_offset" style="padding-bottom: 50px;padding-top: 50px;">
     <div class="container frontpagecontainer" style="margin-top: -50px;">
@@ -211,25 +259,45 @@ $this->load->view('layout/header');
             }
             ?>
         </div>
+        <hr/>
         <!--banners-->
         <section class="row t_xs_align_c">
-            <div class="col-lg-4 col-md-4 col-sm-4 m_bottom_12 m_xs_bottom_30" data-appear-animation="fadeInUp">
-                <a href="#" class="d_block d_xs_inline_b d_mxs_block"><img src="<?php echo base_url(); ?>assets/theme/images/banner_1_1.jpg" alt="Nita Fashions"  width="371" ></a>
+            <div class="col-lg-4 col-md-4 col-sm-4 featureblock" data-appear-animation="fadeInUp">
+                <a href="#" class="d_block d_xs_inline_b d_mxs_block">
+                    <div class='insideblock'>
+                        <img src="<?php echo base_url(); ?>assets/images/delivery.svg" alt="Nita Fashions" >
+                    </div>
+                    <h2>Free & Fast Shipping</h2>
+                    <p>On all order over US$ 250</p>
+                </a>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 m_bottom_12 m_xs_bottom_30" data-appear-animation="fadeInUp" data-appear-animation-delay="200">
-                <a href="#" class="d_block d_xs_inline_b d_mxs_block"><img src="<?php echo base_url(); ?>assets/theme/images/banner_2_1.jpg" alt="Nita Fashions"  width="371" ></a>
+            <div class="col-lg-4 col-md-4 col-sm-4 featureblock" data-appear-animation="fadeInUp" data-appear-animation-delay="200">
+                <a href="#" class="d_block d_xs_inline_b d_mxs_block">
+                    <div class='insideblock'>
+                        <img src="<?php echo base_url(); ?>assets/images/consulting.svg" alt="Nita Fashions">
+                    </div>
+                    <h2>Need Assistance</h2>
+                    <p>Email us at sales@nitafashions.com</p>
+                </a>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 m_bottom_12 m_xs_bottom_0" data-appear-animation="fadeInUp" data-appear-animation-delay="400">
-                <a href="#" class="d_block d_xs_inline_b d_mxs_block"><img src="<?php echo base_url(); ?>assets/theme/images/banner_4_1.jpg" alt="Nita Fashions"  width="371" ></a>
+            <div class="col-lg-4 col-md-4 col-sm-4 featureblock" data-appear-animation="fadeInUp" data-appear-animation-delay="400">
+                <a href="#" class="d_block d_xs_inline_b d_mxs_block">
+                    <div class='insideblock'>
+                        <img src="<?php echo base_url(); ?>assets/images/fabric.svg" alt="Nita Fashions">
+                    </div>
+                    <h2>11,000 Fabrics</h2>
+                    <p>Nita Fashions carry top brands</p>
+                </a>
             </div>
         </section>
+        <hr/>
     </div>
 </section>
 
 <section class="section_offset" style="padding:0px;margin-top: -40px;">
     <div class="container">
+        <h3 class="color_dark fw_light m_bottom_35 heading_1" data-appear-animation="bounceInLeft" style='font-weight: 500;text-align: center;'>Labels We Carry</h3>
 
-        <h3 class="color_dark fw_light m_bottom_15 t_align_c" data-appear-animation="bounceInLeft" style="font-size: 20px; font-weight: 400;">Labels We Carry</h3>
         <!--<p class="m_bottom_35 t_align_c" data-appear-animation="bounceInLeft" data-appear-animation-delay="200" style="margin-bottom: 15px;">Nita Fashions is having Numerous Brands.</p>-->
         <div class="relative" data-appear-animation="bounceInLeft" data-appear-animation-delay="400">
             <div class="t_xs_align_c">
@@ -239,45 +307,45 @@ $this->load->view('layout/header');
                         <div class="row">
 
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
                                     <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/ThomasMasonShort-170x100.jpg" alt="Nita Fashions">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/thomasmason.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
-                                    <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/1_4-170x100.jpg" alt="Nita Fashions">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                    <a href="#" class="d_block  tr_all wrapper r_corners">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/vbc.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>   
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
-                                    <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/1_5-170x100.jpg" alt="Nita Fashions">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                    <a href="#" class="d_block  tr_all wrapper r_corners">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/loropiana.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
-                                    <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/1_6-170x100.jpg" alt="Nita Fashions">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                    <a href="#" class="d_block  tr_all wrapper r_corners">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/zegna.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>
 
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
-                                    <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/1_Holland-Sherry2-170x100.jpg" alt="Nita Fashions">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                    <a href="#" class="d_block  tr_all wrapper r_corners">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/hollandsherry.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>
                             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 m_bottom_20 f_mxs_none w_mxs_full m_mxs_bottom_10">
-                                <div class="clients_item db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
-                                    <a href="#" class="d_block translucent tr_all wrapper r_corners">
-                                        <img src="<?php echo base_url(); ?>assets/theme/images/images/reda.jpg" alt="Nita Fashions">
+                                <div class="clients_item brandlogo db_xs_centered wrapper relative r_corners d_xs_block d_mxs_inline_b">
+                                    <a href="#" class="d_block  tr_all wrapper r_corners">
+                                        <img src="<?php echo base_url(); ?>assets/theme/brand/reda.jpg" alt="Nita Fashions">
                                     </a>
                                 </div>
                             </div>

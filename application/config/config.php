201672f4b84206bb7d015bc4dd89d4c593b321fd
octopuscart <octopuscartltd@gmail.com>
2025-05-15 08:46:33 +0530

    index changes

--- application/config/config.php
+++ application/config/config.php
@@ -59,8 +59,8 @@ $config['rest_enable_keys'] = FALSE;
   | variable so that it is blank.
   |
  */
-$config['index_page'] = strpos($baselink, '192.168') ? 'index.php/' : '';
-$config['index_page'] = '';
+// $config['index_page'] = strpos($baselink, '192.168') ? 'index.php/' : '';
+$config['index_page'] = 'index.php/';
 /*
   |--------------------------------------------------------------------------
   | URI PROTOCOL
